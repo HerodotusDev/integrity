9554028fc071bfae19762a6daaefa1e07bddb99a
Neo <128649481+neotheprogramist@users.noreply.github.com>
2023-12-30 12:06:45 +0100

    runner/1 Basic Cairo Verifier Runner Setup (#18)
    * runner init
    
    * copy derive is zero cost abstraction
    
    * demo of the runner workflow
    
    * running pipeline
    
    * rename
    
    * working flat
    
    * arbitrary nested arrays working
    
    * fix display trait
    
    * enable sierra running
    
    * update readme
    
    * adjust readme
    
    * swap readmes
    
    * delete resources
    
    * add resources dir to .gitignore

---
+++ runner/src/main.rs
@@ -0,0 +1,36 @@
+use std::io::{stdin, Read};
+
+use cairo_args_runner::{run, WrappedArg};
+use clap::Parser;
+use lalrpop_util::lalrpop_mod;
+
+mod ast;
+
+lalrpop_mod!(pub parser);
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// Path to compiled sierra file
+    target: String,
+}
+
+fn main() -> anyhow::Result<()> {
+    let cli = Cli::parse();
+    let mut input = String::new();
+    stdin().read_to_string(&mut input)?;
+
+    let parsed = parser::CairoParserOutputParser::new()
+        .parse(&input)
+        .map_err(|e| anyhow::anyhow!("{}", e))?;
+    let result = format!("{parsed}");
+
+    let target = cli.target;
+    let function = "main";
+    let args: WrappedArg = serde_json::from_str(&result).unwrap();
+
+    let result = run(&target, &function, &args)?;
+
+    println!("{result:?}");
+    Ok(())
+}
