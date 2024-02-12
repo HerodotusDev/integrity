50ee4628638e1c3ab44f056fa45ed30b91f19268
Bartosz Nowak <bartekn337@gmail.com>
2024-02-12 08:16:30 +0100

    OS selection

--- blake.sh
+++ blake.sh
@@ -1,19 +1,43 @@
 #!/bin/bash
 
-find ./src -type f | while read file; do
-    sed -i '/\/\/ === KECCAK ONLY BEGIN ===/,/\/\/ === KECCAK ONLY END ===/ {
-        /\/\/ === KECCAK ONLY BEGIN ===/! {
-            /\/\/ === KECCAK ONLY END ===/! {
-                s/^/\/\/ /
-            }
-        }
-    }' "$file"
+os_type=$(uname -s)
 
-    sed -i '/\/\/ === BLAKE ONLY BEGIN ===/,/\/\/ === BLAKE ONLY END ===/ {
-        /\/\/ === BLAKE ONLY BEGIN ===/! {
-            /\/\/ === BLAKE ONLY END ===/! {
-                s/^\/\/ //
+if [ "$os_type" = "Linux" ]; then
+    find ./src -type f | while read file; do
+        sed -i '/\/\/ === KECCAK ONLY BEGIN ===/,/\/\/ === KECCAK ONLY END ===/ {
+            /\/\/ === KECCAK ONLY BEGIN ===/! {
+                /\/\/ === KECCAK ONLY END ===/! {
+                    s/^/\/\/ /
+                }
             }
-        }
-    }' "$file"
-done
+        }' "$file"
+
+        sed -i '/\/\/ === BLAKE ONLY BEGIN ===/,/\/\/ === BLAKE ONLY END ===/ {
+            /\/\/ === BLAKE ONLY BEGIN ===/! {
+                /\/\/ === BLAKE ONLY END ===/! {
+                    s/^\/\/ //
+                }
+            }
+        }' "$file"
+    done
+elif [ "$os_type" = "Darwin" ]; then
+    find ./src -type f | while read file; do
+        sed -i '' '/\/\/ === KECCAK ONLY BEGIN ===/,/\/\/ === KECCAK ONLY END ===/ {
+            /\/\/ === KECCAK ONLY BEGIN ===/! {
+                /\/\/ === KECCAK ONLY END ===/! {
+                    s/^/\/\/ /
+                }
+            }
+        }' "$file"
+
+        sed -i '' '/\/\/ === BLAKE ONLY BEGIN ===/,/\/\/ === BLAKE ONLY END ===/ {
+            /\/\/ === BLAKE ONLY BEGIN ===/! {
+                /\/\/ === BLAKE ONLY END ===/! {
+                    s/^\/\/ //
+                }
+            }
+        }' "$file"
+    done
+else
+    echo "Unsupported operating system: $os_type"
+fi
