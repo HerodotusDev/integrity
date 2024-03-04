0a9cf37c1a1f9fd0265505cf6888baf95649f8ba
Bartosz Nowak <bartekn337@gmail.com>
2024-03-04 21:00:19 +0100

    runner update

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -4,6 +4,8 @@ use cairo_args_runner::{Arg, Felt252, VecFelt252};
 use clap::Parser;
 
 use cairo_proof_parser::parse;
+use itertools::chain;
+use itertools::Itertools;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -16,21 +18,34 @@ fn main() -> anyhow::Result<()> {
     let args = Cli::parse();
     let mut input = String::new();
     stdin().read_to_string(&mut input)?;
-    let exprs = parse(input)?.to_string();
+    let parsed = parse(input)?;
 
-    let result = run(exprs, args.target)?;
+    let config: VecFelt252 = serde_json::from_str(&parsed.config.to_string()).unwrap();
+    let public_input: VecFelt252 = serde_json::from_str(&parsed.public_input.to_string()).unwrap();
+    let unsent_commitment: VecFelt252 =
+        serde_json::from_str(&parsed.unsent_commitment.to_string()).unwrap();
+    let witness: VecFelt252 = serde_json::from_str(&parsed.witness.to_string()).unwrap();
+
+    let proof = chain!(
+        config.to_vec(),
+        public_input.to_vec(),
+        unsent_commitment.to_vec(),
+        witness.to_vec()
+    )
+    .collect_vec();
+
+    // println!("{:?}", unsent_commitment);
+
+    let target = args.target;
+    let function = "main";
+    
+    let result = cairo_args_runner::run(
+        &target,
+        function,
+        &[Arg::Array(proof)],
+    );
+
     println!("{result:?}");
 
     Ok(())
-}
-
-fn run(parsed: String, target: String) -> anyhow::Result<Vec<Felt252>> {
-    let target = target;
-    let function = "main";
-    let args: VecFelt252 = serde_json::from_str(&parsed).unwrap();
-    Ok(cairo_args_runner::run(
-        &target,
-        function,
-        &[Arg::Array(args.to_vec())],
-    )?)
 }
