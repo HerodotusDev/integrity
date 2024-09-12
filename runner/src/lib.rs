6f050c586f81298c41536e4c93395ea35dc282cc
Filip Krawczyk <kotmawszy@gmail.com>
2024-09-12 21:20:44 +0200

    Adjust runner

--- runner/src/lib.rs
+++ runner/src/lib.rs
@@ -19,3 +19,33 @@ impl From<CairoVersion> for Felt252 {
         }
     }
 }
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+pub enum StoneVersion {
+    Stone5 = 0,
+    Stone6 = 1,
+}
+
+impl From<StoneVersion> for Felt252 {
+    fn from(value: StoneVersion) -> Self {
+        match value {
+            StoneVersion::Stone5 => Felt252::from(0),
+            StoneVersion::Stone6 => Felt252::from(1),
+        }
+    }
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+pub enum HasherBitLength {
+    Lsb160 = 0,
+    Lsb248 = 1,
+}
+
+impl From<HasherBitLength> for Felt252 {
+    fn from(value: HasherBitLength) -> Self {
+        match value {
+            HasherBitLength::Lsb160 => Felt252::from(0),
+            HasherBitLength::Lsb248 => Felt252::from(1),
+        }
+    }
+}
